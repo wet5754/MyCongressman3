bab8fe7c39a2cacf9add192ed50843120767d23c
ksh04023 <ksh04023@gmail.com>
2020-02-24 06:02:24 +0900

    에러 개많음,,^^ 탭 두번누르면안댐

--- app/src/main/java/com/example/mycongressman3/MainActivity.kt
+++ app/src/main/java/com/example/mycongressman3/MainActivity.kt
@@ -3,18 +3,25 @@ package com.example.mycongressman3
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.get
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
-
+    lateinit var fragmentManager: FragmentManager
+    lateinit var transaction: FragmentTransaction
+    val tab1Fragment = Tab1Fragment()
+    val tab2Fragment = Tab2Fragment()
+    val tab3Fragment = Tab3Fragment()
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,30 +34,47 @@ class MainActivity : AppCompatActivity() {
 //        val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send
-            ), drawerLayout
-        )
+
 //        setupActionBarWithNavController(navController, appBarConfiguration)
 //        navView.setupWithNavController(navController)
-
+        fragmentManager = this.supportFragmentManager
+        transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.bottom_tab_frame, tab1Fragment,"tab1").commitAllowingStateLoss()
         init()
     }
-    fun init(){
+
+    fun init() {
         var button_search = b_search
         button_search.setOnClickListener {
             val intent = Intent(this, SearchActivity::class.java)
             startActivity(intent)
         }
-        bottom_tab.setOnNavigationItemReselectedListener {
-            if (bottom_tab[0].isEnabled){
-                Toast.makeText(applicationContext, "tab1",Toast.LENGTH_SHORT).show() //이제 프레임넣는거!
-            }
-        }
+        bottom_tab.setOnNavigationItemSelectedListener {
+            val transaction = fragmentManager.beginTransaction()
+            when (it.itemId) {
+                R.id.b_timeline -> {
+                    var selectedFragment = tab1Fragment
+                    transaction.add(R.id.bottom_tab_frame, selectedFragment)
 
+                }
+                R.id.b_trending -> {
+                    var selectedFragment = tab2Fragment
+                    transaction.add(R.id.bottom_tab_frame, selectedFragment)
+
+
+                }
+                R.id.b_my_page -> {
+                    var selectedFragment = tab3Fragment
+                    transaction.add(R.id.bottom_tab_frame, selectedFragment)
+
+
+                }
+            }
+            transaction.commit()
+            true
+        }
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
