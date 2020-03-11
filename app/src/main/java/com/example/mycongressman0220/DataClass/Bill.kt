ded1043fcfbef36a4a5568d22fbfb4c85a5f18af
ksh04023 <ksh04023@gmail.com>
2020-03-12 02:41:24 +0900

    Tab1Fragment 성공!

--- app/src/main/java/com/example/mycongressman0220/DataClass/Bill.kt
+++ app/src/main/java/com/example/mycongressman0220/DataClass/Bill.kt
@@ -1,7 +1,11 @@
 package com.example.mycongressman0220.DataClass
 
-data class Bill (
-    var billID:String,
+import android.annotation.SuppressLint
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Bill(
+    var billID: String,
     var dataTitle:String,
     var title:String,
     var date:String,
@@ -9,4 +13,43 @@ data class Bill (
     var content:String,
     var coactor:String,
     var if_processed:Boolean
-)
+):Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readByte() != 0.toByte()
+    ) {
+    }
+
+    @SuppressLint("NewApi")
+    override fun writeToParcel(p0: Parcel?, p1: Int) {
+        p0?.writeString(billID)
+        p0?.writeString(dataTitle)
+        p0?.writeString(title)
+        p0?.writeString(date)
+        p0?.writeString(link)
+        p0?.writeString(content)
+        p0?.writeString(coactor)
+        p0?.writeBoolean(this.if_processed)
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Bill> {
+        override fun createFromParcel(parcel: Parcel): Bill {
+            return Bill(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Bill?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/example/mycongressman0220/Tab1Adapter.kt
+++ app/src/main/java/com/example/mycongressman0220/Tab1Adapter.kt
@@ -1,48 +1,51 @@
-package com.example.mycongressman0220.Tab1
+package com.example.mycongressman0220
 
-import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mycongressman0220.DataClass.Bill
-import com.example.mycongressman0220.R
-import com.example.mycongressman0220.Tab1.Tab1Adapter.ViewHolder
 
-class Tab1Adapter(var context: Context, var items: ArrayList<Bill>):RecyclerView.Adapter<ViewHolder>(){
-    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+class Tab1Adapter(var items:ArrayList<Bill>): RecyclerView.Adapter<Tab1Adapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         var pic = itemView.findViewById<ImageView>(R.id.tab1_card_pic)
         var name = itemView.findViewById<TextView>(R.id.tab1_card_name)
         var date = itemView.findViewById<TextView>(R.id.tab1_card_date)
         var title = itemView.findViewById<TextView>(R.id.tab1_card_title)
         var content = itemView.findViewById<TextView>(R.id.tab1_card_content)
-        var more = itemView.findViewById<Button>(R.id.tab1_card_more)
+        var more = itemView.findViewById<ImageButton>(R.id.tab1_card_more)
+
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(context).inflate(R.layout.tab1_card_layout, parent, false)
-        Log.d("tab1","viewholder made")
+        val v =
+            LayoutInflater.from(parent.context).inflate(R.layout.tab1_card_layout, parent, false)
+        Log.d("tab1", "viewholder made")
         return ViewHolder(v)
     }
 
-    override fun getItemCount(): Int {
-        return items.size
-    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = items.get(position)
+        var item = items.get(position)
         holder.date.setText(item.date)
         holder.title.setText(item.title)
         holder.content.setText(item.content)
-        holder.more.setOnClickListener {
-            //TODO
-        }
-        Log.d("recycleriew","successful")
+
+//        var tab1Fragment = Tab1Fragment()
+//        var bundle = Bundle()
+//        bundle.putParcelable(item)
+//
+//
+//        Log.d("recycleriew","successful")
     }
-
-
-
+    override fun getItemCount(): Int {
+        return items.size
+    }
 }
+
+
+
